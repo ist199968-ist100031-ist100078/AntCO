3e48e135ff2dd080505a1fc3216e7832c4805154
Joao-Ex-Machina <joaobarreiroscoelhorodrigues@tecnico.ulisboa.pt>
2023-06-04 17:08:05 +0100

    Reworked Pheromone class nomenclature. Added Ant class. Added comments

--- src/AntCO/Ant.java
+++ src/AntCO/Ant.java
@@ -1,5 +1,110 @@
 package AntCO;
-
+package graph;
+/* Name: Ant
+description: Ant class for Ant Colony Optimization
+Date added: 04 Jun 2023
+Last modified: 04 Jun 2023
+*/
 public class Ant {
-    
+int path[];
+    int pathlength;
+    int maxvertex;
+    int start;
+    int end;
+    int current;
+    int next;
+    int visited[];
+    int unvisited[];
+    int unvisitedlength;
+    int visitedlength;
+    int sigma; /*sum of all weights*/
+    int weight;
+    float gamma;
+    float rho;
+    Pheromone pheromone;
+    IWeightedGraph graph;
+
+    /*Constructor*/
+    public Ant(int max, int start, int end, float gamma, float rho, Pheromone pheromone, IWeightedGraph graph) {
+        this.maxvertex = max;
+        this.start = start;
+        this.end = end;
+        this.gamma = gamma;
+        this.rho = rho;
+        this.pheromone = pheromone;
+        this.graph = graph;
+        this.path = new int[maxvertex];
+        this.visited = new int[maxvertex];
+        this.unvisited = new int[maxvertex];
+        this.pathlength = 0;
+        this.visitedlength = 0;
+        this.unvisitedlength = maxvertex;
+        this.sigma = 0;
+        this.weight = 0;
+        this.current = start;
+        this.next = -1;
+        for (int i=0; i<maxvertex; i++) {
+            this.visited[i] = -1;
+            this.unvisited[i] = i;
+        }
+        this.visited[0] = start;
+        this.unvisited[start] = -1;
+        this.unvisitedlength--;
+    }
+
+    /* Name: move
+    input: none
+    output: none
+    description: move the ant to the next vertex and update stored path
+    Date added: 04 Jun 2023
+    Last modified: 04 Jun 2023
+    */
+    public void move() {
+        this.next = this.selectNext();
+        this.path[this.pathlength] = this.next;
+        this.pathlength++;
+        this.visited[this.visitedlength] = this.next;
+        this.visitedlength++;
+        this.unvisited[this.next] = -1;
+        this.unvisitedlength--;
+        this.sigma +=this.graph.getCost(this.current, this.next) ;
+        this.current = this.next;
+    }
+
+    /* Name: pheromonoize
+    input: none
+    output: none
+    description: pheromonize all edges in the stored path
+    Date added: 04 Jun 2023
+    Last modified: 04 Jun 2023
+    */
+    public void pheromonize(){
+        for (int i=0; i<this.pathlength-1; i++) {
+            this.pheromone.incrementFvalue(this.path[i], this.path[i+1], this.gamma,this.sigma , this.graph.getCost(i,j));
+        }
+    }
+
+    /* Name: move
+    input: none
+    output: none
+    description: move the ant to the next vertex and update stored path
+    Date added: 04 Jun 2023
+    Last modified: 04 Jun 2023
+    */
+    public void reset() {
+        this.pathlength = 0;
+        this.visitedlength = 0;
+        this.unvisitedlength = this.maxvertex;
+        this.sigma = 0;
+        this.weight = 0;
+        this.current = this.start;
+        this.next = -1;
+        for (int i=0; i<maxvertex; i++) {
+            this.visited[i] = -1;
+            this.unvisited[i] = i;
+        }
+        this.visited[0] = this.start;
+        this.unvisited[this.start] = -1;
+        this.unvisitedlength--;
+    }
 }
--- src/AntCO/Pheromone.java
+++ src/AntCO/Pheromone.java
@@ -1,27 +1,46 @@
 package AntCO;
 
+/* Name: Pheromone
+description: Pheromone class for Ant Colony Optimization
+Date added: 03 Jun 2023
+Last modified: 04 Jun 2023
+*/
 public class Pheromone {
-    float value[][]; //pheromone value
+    float fvalue[][]; //pheromone value
     int maxvertex;
-
+    /*Constructor*/
     public Pheromone(int max) {
         this.maxvertex = max;
-        this.fvalue= new float[vertex][vertex];
+        this.fvalue= new float[maxvertex][maxvertex];
         Arrays.fill(this.fvalue, 0);
     }
-
+    /*getter*/
     public float getFvalue(int i, int j) {
         return this.fvalue[i][j];
     }
 
+    /* Name: decayFvalue
+    input: rho
+    output: none
+    description: decay the all pheromone values by rho
+    Date added: 03 Jun 2023
+    Last modified: 04 Jun 2023
+    */
     public void decayFvalue(float rho) {
         for (int i=0; i<maxvertex; i++)
             for (int j=0; j<maxvertex; j++)
-                    this.fvalue = fvalue-rho;
+                    this.fvalue[i][j] = fvalue[i][j]-rho;
     }
 
-    public void incrementFvalue(int i, int j, float gamma, int sigma-mu, int weight) {
-        this.fvalue[i][j] += gamma*weight/sigma-mu;
+    /* Name: incrementFvalue
+    input: i, j, gamma, sigma, weight
+    output: none
+    description: increment the pheromone value of edge (i,j)
+    Date added: 03 Jun 2023
+    Last modified: 04 Jun 2023
+     */
+    public void incrementFvalue(int i, int j, float gamma, int sigma, int weight) {
+        this.fvalue[i][j] += gamma*weight/sigma;
     }
 
 }
