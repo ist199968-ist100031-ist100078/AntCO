2ee798a3c66621dd5c78971e6aab27a6c80a3eb8
Martim <80010382+G05B3@users.noreply.github.com>
2023-06-10 12:48:40 +0100

    Update Edge.java

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -1,1 +1,44 @@
-package graph;public class Edge implements Comparable<Edge>{		private int weight;	private Vertex na, nb;		int get_weight() {		return weight;	}		Vertex[] get_nodes() {		Vertex[] arr = {na, nb};		return arr;	}		private void buildEdge(Vertex a, Vertex b, int w) {		if (a.getId() < b.getId()) {			this.na = a;			this.nb = b;		}		else {			this.na = b;			this.nb = a;		}		this.weight = w;	}		public Edge(Vertex a, Vertex b, int w) {		buildEdge(a, b, w);	}		public Edge(Vertex a, Vertex b) {		buildEdge(a, b, 1);	}		public int compareTo(Edge e1) {				Integer c = Integer.compare(this.na.getId(), e1.get_nodes()[0].getId());		if (c.equals(0)) {			return Integer.compare(this.nb.getId(), e1.get_nodes()[1].getId());		}		return c;	}}
+package graph;
+
+public class Edge implements Comparable<Edge> {
+	
+	private int weight;
+	private Vertex na, nb;
+	
+	int get_weight() {
+		return weight;
+	}
+	
+	Vertex[] get_nodes() {
+		Vertex[] arr = {na, nb};
+		return arr;
+	}
+	
+	private void buildEdge(Vertex a, Vertex b, int w) {
+		if (a.getId() < b.getId()) {
+			this.na = a;
+			this.nb = b;
+		}
+		else {
+			this.na = b;
+			this.nb = a;
+		}
+		this.weight = w;
+	}
+	
+	public Edge(Vertex a, Vertex b, int w) {
+		buildEdge(a, b, w);
+	}
+	
+	public Edge(Vertex a, Vertex b) {
+		buildEdge(a, b, 1);
+	}
+	
+	public int compareTo(Edge e1) {		
+		Integer c = Integer.compare(this.na.getId(), e1.get_nodes()[0].getId());
+		if (c.equals(0)) {
+			return Integer.compare(this.nb.getId(), e1.get_nodes()[1].getId());
+		}
+		return c;
+	}
+}
