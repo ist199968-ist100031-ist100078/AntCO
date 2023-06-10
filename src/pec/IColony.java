a1a3ec88aad1919786011153dfefa1eee7e9274d
Rodrigo Coimbra <rodscoimbra@gmail.com>
2023-06-10 12:56:51 +0100

    Delete IColony.java

--- src/pec/IColony.java
+++
@@ -1,9 +0,0 @@
-package pec;
-
-import java.util.ArrayList;
-
-public interface IColony {
-    public int getCost(int hashedge);
-    public ArrayList<Integer> triggerAntMovement(int triggerid);
-    public boolean triggerPheromoneDecay(int hashededge);
-}
